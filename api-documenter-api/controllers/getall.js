e63356559fc19e415527a9ed4b2b212b3a8a37f4
andem20 <andem20@student.sdu.dk>
2021-09-25 20:07:05 +0200

    removed some text in error message

--- api-documenter-api/controllers/getall.js
+++ api-documenter-api/controllers/getall.js
@@ -3,7 +3,7 @@ const Schemas = require('../models/api_model');
 const getall = (req, res) => {
     Schemas.Api.find({}, function(err, result) {
         if (err) {
-            console.log(err + "hej");
+            console.log(err);
         } else {
             res.json(result);
         }
