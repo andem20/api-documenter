343dd78aaf733e2c21691bd01914400853e93978
andem20 <andem20@student.sdu.dk>
2021-09-25 19:56:16 +0200

    Small changes

--- api-documenter-api/controllers/getall.js
+++ api-documenter-api/controllers/getall.js
@@ -3,9 +3,9 @@ const Schemas = require('../models/api_model');
 const getall = (req, res) => {
     Schemas.Api.find({}, function(err, result) {
         if (err) {
-        console.log(err);
+            console.log(err);
         } else {
-        res.json(result);
+            res.json(result);
         }
     });
 }
